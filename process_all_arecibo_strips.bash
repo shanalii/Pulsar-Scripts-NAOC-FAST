8c2b0c6c14ea02cddaf514f828b0a4b9c097ecfb
shanalii <sli2@oberlin.edu>
2017-07-13 13:45:55 +0800

    Run all Arecibo data on node in blocks

---
+++ process_all_arecibo_strips.bash
@@ -0,0 +1,189 @@
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_1 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_2 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_3 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_4 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_5 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_6 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_7 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_8 &
+wait
+
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_9 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_10 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_11 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_12 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_13 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_14 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_15 &
+wait
+
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_16 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_17 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_18 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_19 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_20 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_21 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_22 &
+wait
+
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_23 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_24 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_25 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_26 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_27 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_28 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_29 &
+wait
+
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_30 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_31 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_32 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_33 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_34 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_35 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_36 &
+wait
+
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_37 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_38 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_39 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_40 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_41 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_42 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_43 &
+wait
+
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_44 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_45 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_46 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_47 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_48 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_49 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_50 &
+wait
+
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_51 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_52 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_53 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_54 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_55 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_56 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_57 &
+wait
+
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_58 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_59 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_60 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_61 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_62 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_63 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_64 &
+wait
+
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_65 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_66 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_67 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_68 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_69 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_70 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_71 &
+wait
+
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_72 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_73 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_74 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_75 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_76 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_77 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_78 &
+wait
+
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_79 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_80 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_81 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_82 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_83 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_84 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_85 &
+wait
+
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_86 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_87 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_88 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_89 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_90 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_91 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_92 &
+wait
+
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_93 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_94 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_95 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_96 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_97 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_98 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_99 &
+wait
+
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_100 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_101 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_102 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_103 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_104 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_105 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_106 &
+wait
+
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_107 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_108 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_109 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_110 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_111 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_112 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_113 &
+wait
+
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_114 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_115 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_116 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_117 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_118 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_119 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_120 &
+wait
+
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_121 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_122 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_123 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_124 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_125 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_126 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_127 &
+wait
+
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_128 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_129 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_130 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_131 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_132 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_133 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_134 &
+wait
+
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_135 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_136 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_137 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_138 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_139 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_140 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_141 &
+wait
+
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_142 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_143 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_144 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_145 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_146 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_147 &
+bash ~/ddscript_arecibo.bash ~/arecibo_cut Arecibo_Out_148 &
+wait
